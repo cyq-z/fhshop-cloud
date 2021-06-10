b0db617b535ef360aea72f54c3e10d74e40e2e21
cyqjava <2833952998@qq.com>
2021-06-10 14:00:48 +0800

    添加注册中心内容

--- shop-reg-center/src/main/java/com/fh/shop/ShopRegCenterApp.java
+++ shop-reg-center/src/main/java/com/fh/shop/ShopRegCenterApp.java
@@ -4,11 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+/**
+ * @author love
+ */
 @SpringBootApplication
 @EnableEurekaServer
 public class ShopRegCenterApp {
 
     public static void main(String[] args) {
+        System.out.println("**********");
         SpringApplication.run(ShopRegCenterApp.class,args);
     }
 }
