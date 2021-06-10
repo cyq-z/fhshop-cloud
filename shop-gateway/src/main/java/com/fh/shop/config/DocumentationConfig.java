832ed51443d90aacbfb3c36c1d9f3232fb8d7e0e
cyq-z <2833952998@qq.com>
2021-06-10 19:03:22 +0800

    today work

--- shop-gateway/src/main/java/com/fh/shop/config/DocumentationConfig.java
+++ shop-gateway/src/main/java/com/fh/shop/config/DocumentationConfig.java
@@ -1,5 +1,7 @@
 package com.fh.shop.config;
 
+import org.springframework.cloud.netflix.zuul.filters.Route;
+import org.springframework.cloud.netflix.zuul.filters.RouteLocator;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 import springfox.documentation.swagger.web.SwaggerResource;
@@ -16,13 +18,21 @@ import java.util.List;
 @Component
 @Primary
 public class DocumentationConfig  implements SwaggerResourcesProvider {
+
+    private final RouteLocator routeLocator;
+
+    public DocumentationConfig(RouteLocator routeLocator) {
+        this.routeLocator = routeLocator;
+    }
     @Override
     public List<SwaggerResource> get() {
         List resources = new ArrayList<>();
+        List<Route> routes=routeLocator.getRoutes();
+        routes.forEach(route -> {
+            resources.add(swaggerResource(route.getId(),route.getFullPath().replace("**","v2/api-docs"),"1.0"));
+        });
         //api/cates/是网关路由，/v2/api-docs是swagger中的
-        resources.add(swaggerResource("shop-cate-api", "/cate/v2/api-docs", "1.0"));
-        resources.add(swaggerResource("shop-goods-api", "/goods/v2/api-docs", "1.0"));
-        resources.add(swaggerResource("shop-member-api", "/member/v2/api-docs", "1.0"));
+//        resources.add(swaggerResource("shop-member-api", "/member/v2/api-docs", "1.0"));
         return resources;
     }
 
